3b5fbcaf32111f97e28432f47b7855e0fdd94f04
abhay333d <abhaydandge333@gmail.com>
2024-08-25 14:16:17 +0530

    added the atmosphere, added it to gui, used fresnel to get the vlaue which goes from 0 to 1, mixed the color + atmosphereColor + fresnel * atmosphereDayColor. to get the perfect atmosphere on the day side and not on the night side

--- src/script.js
+++ src/script.js
@@ -22,6 +22,20 @@ const textureLoader = new THREE.TextureLoader();
 /**
  * Earth
  */
+const earthParameters = {};
+earthParameters.atmosphereDayColor = "#00aaff";
+earthParameters.atmosphereTwilightColor = "#ff6600";
+gui.addColor(earthParameters, "atmosphereDayColor").onChange(() => {
+  earthMaterial.uniforms.uAtmosphereDayColor.value.set(
+    earthParameters.atmosphereDayColor
+  );
+});
+gui.addColor(earthParameters, "atmosphereTwilightColor").onChange(() => {
+  earthMaterial.uniforms.uAtmosphereTwilightColor.value.set(
+    earthParameters.atmosphereTwilightColor
+  );
+});
+
 //Textures
 const earthDayTexture = textureLoader.load("./earth/day.jpg");
 earthDayTexture.colorSpace = THREE.SRGBColorSpace;
@@ -44,6 +58,12 @@ const earthMaterial = new THREE.ShaderMaterial({
     uNightTexture: new THREE.Uniform(earthNightTexture),
     uSpecularCloudsTexture: new THREE.Uniform(earthSpecularCloudsTexture),
     uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
+    uAtmosphereDayColor: new THREE.Uniform(
+      new THREE.Color(earthParameters.atmosphereDayColor)
+    ),
+    uAtmosphereTwilightColor: new THREE.Uniform(
+      new THREE.Color(earthParameters.atmosphereTwilightColor)
+    ),
   },
 });
 const earth = new THREE.Mesh(earthGeometry, earthMaterial);
